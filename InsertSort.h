abc08b0fd0595a49fd652c0ba49722aa802d2188
Davitttt <167617785+LUISTHEREAL@users.noreply.github.com>
2026-04-19 23:07:37 +0300

    Enhance comments in InsertSort.h
    Added comments to clarify the insertion sort logic.

--- InsertSort.h
+++ InsertSort.h
@@ -6,10 +6,13 @@ void insertionSort(vector<int>& numb, int left, int right)
     int j;
     for (int i = left + 1; i <= right; i++)
     {
-        int temp = numb[i];  
+        //saves the current
+        int temp = numb[i];
+         //shifts to the bigger one also one step to the right
         for (j = i; j > left && temp < numb[j - 1]; j--) {
             numb[j] = numb[j - 1];
         };
+        //places it in the correct spot
         numb[j] = temp;
 
     }
